2e8bbefc815b8d8c774de35393abc691d000518d
Hannah Carney <hannahcarneyart@gmail.com>
2014-12-07 18:15:33 +0000

    Added user input

--- directory.rb
+++ directory.rb
@@ -28,6 +28,27 @@ def print(students)
 end
 #nothing happens until we call the methods
 
+
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	#create an empty array
+	students =[]
+	#gets first name
+	name = gets.chomp
+	#while the name is not empty, repeat this code
+	while !name.empty? do
+	#add the student hash to the aray
+	students << {:name => name, :cohort => :november}
+	puts "Now we have #{students.length} students"
+    #get another name from the user
+    name = gets.chomp
+end
+#return the array of students
+students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
